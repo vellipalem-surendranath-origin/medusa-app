5ad9ca5acb7b50285b654f4beb001578e97d9c66
azuredevopss <87042366+azuredevopss@users.noreply.github.com>
2024-09-03 14:27:58 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 module "vpc" {
   source = "./vpc"
 }
