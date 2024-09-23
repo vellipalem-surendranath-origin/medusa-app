ecfb4770fcebb0c078b356ff42dc3f0eeff46349
azuredevopss <87042366+azuredevopss@users.noreply.github.com>
2024-09-22 21:38:32 -0500

    Update redis.tf

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -3,25 +3,15 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   subnet_ids = ["subnet-0c9b473d9397d8d61"]
 }
 
-
-resource "aws_elasticache_replication_group" "default" {
-  replication_group_id          = "medusa-redis"
-  replication_group_description = "Redis cluster for medusa backend"
-
+resource "aws_elasticache_cluster" "medusa" {
+  cluster_id           = "medusa-redis"
+  engine               = "redis"
   node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
   port                 = 6379
-  parameter_group_name = "default.redis3.2.cluster.on"
-
-  snapshot_retention_limit = 5
-  snapshot_window          = "00:00-05:00"
-
   subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
-  automatic_failover_enabled = true
-
-  cluster_mode {
-    replicas_per_node_group = 1
-    num_node_groups         = 3
-  }
   depends_on = [
     aws_elasticache_subnet_group.redis_subnet_group
   ]
