07cbd3e681f533fdc73a468f070c8a1659747922
azuredevopss <87042366+azuredevopss@users.noreply.github.com>
2024-09-22 21:49:04 -0500

    Update redis.tf

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -8,7 +8,7 @@ resource "aws_elasticache_cluster" "medusa" {
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "7.2"
   engine_version       = "3.2.10"
   port                 = 6379
   subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
