89ef73ec7581f833c6f605722f47b6cb3840998b
azuredevopss <87042366+azuredevopss@users.noreply.github.com>
2024-09-22 19:39:41 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,10 +14,9 @@ terraform {
 terraform {
   backend "s3" {
     bucket         = "medusa-terraform-state-bucket"
-    key            = "medusa-terraform.tfstate"
-    region         = "us-east-1"
+    key            = "medusa-ecs-terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
-
