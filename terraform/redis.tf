9d2470a16f51147825b754d560d87b7b5ccd3318
azuredevopss <87042366+azuredevopss@users.noreply.github.com>
2024-09-22 22:30:09 -0500

    Update redis.tf

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,6 +1,6 @@
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = "redis-subnet-group"
-  subnet_ids = ["subnet-0c9b473d9397d8d61"]
+  subnet_ids = ["subnet-0c9b473d9397d8d61","subnet-0de0da3c4f069e929","subnet-01e20ef74422cd971"]
 }
 
 resource "aws_elasticache_cluster" "medusa" {
