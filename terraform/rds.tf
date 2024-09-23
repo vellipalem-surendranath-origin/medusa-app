a90d90e00a9ec8c8753e244f6fbc7cc5e9b8e408
azuredevopss <87042366+azuredevopss@users.noreply.github.com>
2024-09-22 22:14:12 -0500

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,3 +1,9 @@
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "medusa-db-subnet-group"
+  subnet_ids = ["subnet-0c9b473d9397d8d61"]
+  description = "Subnet group for Medusa PostgreSQL RDS"
+}
+
 resource "aws_db_instance" "medusa_db" {
   allocated_storage    = 20
   engine               = "postgres"
@@ -9,9 +15,6 @@ resource "aws_db_instance" "medusa_db" {
   parameter_group_name = "default.postgres16"
   publicly_accessible  = true
   vpc_security_group_ids = [aws_security_group.medusa_sg.id]
-  db_subnet_group_name = aws_elasticache_subnet_group.redis_subnet_group.name
-  depends_on = [
-    aws_elasticache_subnet_group.redis_subnet_group
-  ]
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
 }
 
